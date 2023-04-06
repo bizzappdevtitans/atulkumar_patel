dcec769ba9bb12f1f0e4cdd9a67053f0a607caa4
bizzappdevtitans <96333187+bizzappdevtitans@users.noreply.github.com>
2023-04-06 18:19:05 +0530

    Delete sale_inheritance directory

--- sale_inheritance/models/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class AccountMove(models.Model):
-    '''Inherit account move model #T00316'''	
-
-    _inherit = "account.move"
-    _description = "sale inheritance account move"
-
-    invoice_text = fields.Text(string="Invoice Description")
--- sale_inheritance/models/project_project.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class ProjectProject(models.Model):
-	''' inherit project.project model #T00316'''
-	_inherit = "project.project"
-	_description = "sale inheritance project_project"
-
-	project_description = fields.Text(string="Project Description")
